5061a2cde8d12fd03e654cb13bcab52ae9a24dd6
Waren Gador <123795328+Renstrio24p@users.noreply.github.com>
2023-05-28 05:20:41 +0800

    add all

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import ReactApp from "./react-18/react";
 import "https://kit.fontawesome.com/6b203b1712.js";
 import "./index.css";
 import Start from "./start"; 
-import { Store } from "./ToDo-List/storage/store";
 
 const DOM = ReactDOM.createRoot(document.getElementById("root"));
 DOM.render(
     <React.StrictMode>
-        <Provider store={Store}>
-            <Start />
-        </Provider>
+            <ReactApp />
     </React.StrictMode>
 );
--- src/start.jsx
+++ src/start.jsx
@@ -1,30 +1,10 @@
 import React from "react";
-import TodoPage from "./ToDo-List/component/Page";
-import Header from "./ToDo-List/component/Header";
-import style from "./ToDo-List/component/styles/modules/app.module.scss";
-import Content from "./ToDo-List/component/Content";
-import { Toaster } from "react-hot-toast";
 
 export default function Start() {
   return (
-    <>
-      <div className="container">
-        <TodoPage>
-          To Do <i className="fa-solid fa-list"></i>
-        </TodoPage>
-        <div className={style.app__wrapper}>
-          <Header />
-          <Content></Content>
-        </div>
+      <div className="App">
+        <h1>Hello React <i className="fa-solid fa-handshake"></i></h1>
       </div>
-          <Toaster
-            position="bottom-left"
-          toastOptions={{
-            style:{
-                fontSize: '1.4rem',
-            }
-          }}/>
-    </>
   );
 }
 
